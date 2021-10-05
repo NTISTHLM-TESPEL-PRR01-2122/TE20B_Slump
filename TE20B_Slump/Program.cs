e143b065d0dce53c87e5c3d9c38173cd1cc0ecaa
Mikael Bergström <mikael.bergstrom@ga.ntig.se>
2021-10-05 11:08:07 +0200

    Initial commit

---
+++ TE20B_Slump/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+Random generator = new Random();
+
+int damage = generator.Next(50, 100);
+
+Console.WriteLine(damage);
+Console.WriteLine(damage);
+Console.WriteLine(damage);
+Console.WriteLine(damage);
+
+
+Console.ReadLine();
